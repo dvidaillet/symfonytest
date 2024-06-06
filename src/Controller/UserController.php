e455827c033de254c613ac3300dd60af038182a0
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-06-06 14:41:36 -0500

    add user form

---
+++ src/Controller/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('user_success');
+        }
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+        
+    }
+
+    #[Route('/user/success', name: 'user_success')]
+    public function success(): Response
+    {
+        return new Response('User created successfully!');
+    }
+}
